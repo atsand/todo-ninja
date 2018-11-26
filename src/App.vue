9a11206f8207cf010533c7073079df512ad248de
Aaron <atsand23@gmail.com>
2018-11-26 17:15:59 -0500

    Start of home page
    added nav bar and side pane

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-app>        
+  <v-app class="grey lighten-4">
+    <Navbar/>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -7,10 +8,12 @@
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 
 export default {
   name: 'App',
   components: {
+    Navbar
   },
   data () {
     return {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <v-toolbar app>
+      <v-toolbar-side-icon class="grey--text" v-on:click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="grey--text text-uppercase">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <v-btn slot="activator" fab small flat color="grey">
+          <v-icon>logout</v-icon>
+        </v-btn>
+        <span>Sign Out</span>
+      </v-tooltip>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-title-content>
+            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+          </v-list-title-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      drawer: false,
+      links: [
+        {icon: 'dashboard', text: 'Dashboard', route:'/'},
+        {icon: 'folder', text: 'My Projects', route:'/projects'},
+        {icon: 'person', text: 'Team', route:'/team'}
+      ]
+    }
+  }
+}
+</script>
