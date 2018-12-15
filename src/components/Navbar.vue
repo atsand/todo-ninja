a9d9b567a5d288b3a85201ff6227e64e9cebec26
Aaron <atsand23@gmail.com>
2018-12-15 18:43:08 -0500

    Added new project button and popup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -36,6 +36,9 @@
           </v-avatar>
           <p class="white--text subheading mt-1">The Net Ninja</p>
         </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup></Popup>
+        </v-flex>
       </v-layout>
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
@@ -52,7 +55,10 @@
 </template>
 
 <script>
+import Popup from './Popup.vue'
+
 export default{
+  components:{Popup},
   data(){
     return{
       drawer: false,
---
+++ src/components/Popup.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-dialog max-width="600px">
+        <v-btn slot="activator" flat class="success">Add New Project</v-btn>
+        <v-card>
+            <v-card-title>
+                <h2>Add a New Project</h2>
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default{
+
+}
+</script>
