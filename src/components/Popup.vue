323f6cd3651a53554335f04d55ebfbdf021c4f3b
Aaron <atsand23@gmail.com>
2018-12-15 21:17:54 -0500

    Date formatting and date picker

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,12 +5,41 @@
             <v-card-title>
                 <h2>Add a New Project</h2>
             </v-card-title>
+            <v-card-text>
+                <v-form class="px-3">
+                    <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+                    <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+                    <v-menu>
+                        <v-text-field slot="activator" label="Due Date" prepend-icon="date_range" :value="formattedDate"></v-text-field>
+                        <v-date-picker v-model="due"></v-date-picker>
+                    </v-menu>
+                    <v-spacer></v-spacer>
+                    <v-btn flat class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
+                </v-form>
+            </v-card-text>
         </v-card>
     </v-dialog>
 </template>
 
 <script>
-export default{
-
+import moment from 'moment'
+export default {
+    data(){
+        return{
+            title:'',
+            content:'',
+            due:null
+        }
+    },
+    methods: {
+        submit() {
+            alert(this.title + this.content)
+        }
+    },
+    computed:{
+        formattedDate(){
+            return this.due ? moment(this.due).format('Do MMM YYYY'): '';
+        }
+    }
 }
 </script>
